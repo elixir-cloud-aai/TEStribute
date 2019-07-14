e50dca8f4814aad6cb82d65710372e8943c90ff3
vaniisgh <vanisingh@live.co.uk>
2019-07-14 22:10:41 +0400

    add drs_module
    add GetObject for DRS objects

--- TEStribute/ClientWrapper.py
+++ TEStribute/ClientWrapper.py
@@ -1,26 +1,15 @@
-import tes_client.Client
-import drs_client.Client
+from drs_client_module import fetchDataObject
+from tes_client_module import fetchTasksInfo
 
-tes_url = 'http://193.166.24.111/ga4gh/tes/v1/'
-drs_url = 'http://193.166.24.114/ga4gh/drs/v1'
-
-
-def fetchTasksInfo(tes_url, cpu_cores, ram_gb, disk_gb, preemtible, zones, execution_time_min):
-    c = tes_client.Client.Client(tes_url)
-    return c.GetTaskInfo(cpu_cores, ram_gb, disk_gb, preemtible, zones, execution_time_min)
-
-
-def fetchDRSInfo(drs_url, object_id):
-    drs_client.Client.Client(drs_url)
-    c = drs_client.Client.Client(drs_url)
-    return c.GetObject(object_id)
+tes_url = "http://193.166.24.111/ga4gh/tes/v1/"
+drs_url = "http://193.166.24.114/ga4gh/drs/v1"
 
 
 if __name__ == "__main__":
     """
     use functions defined
     """
-    TesInfo = fetchTasksInfo(tes_url,4,8,10,True,[],20)
-    DRS_response = fetchDRSInfo(drs_url, 'a001')
-
-    print(TesInfo)
+    DRS_response = fetchDataObject(drs_url, "a001")
+    TES_response = fetchTasksInfo(tes_url, 4, 8, 10, True, [], 12)
+    print(TES_response)
+    print(DRS_response)
--- setup.py
+++ setup.py
@@ -1,28 +1,27 @@
-from setuptools import (setup, find_packages)
+from setuptools import setup, find_packages
 
-with open('README.md', 'r') as fh:
+with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name='TEStribute',
-    version='0.1.0',
-    author='Elixir Europe',
-    author_email='alexander.kanitz@alumni.ethz.ch',
-    description='LB for TES endpoints',
+    name="TEStribute",
+    version="0.1.0",
+    author="Elixir Europe",
+    author_email="alexander.kanitz@alumni.ethz.ch",
+    description="LB for TES endpoints",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    license='Apache License 2.0',
-    url='https://github.com/elixir-europe/TEStribute',
+    license="Apache License 2.0",
+    url="https://github.com/elixir-europe/TEStribute",
     packages=find_packages(),
-    keywords=(
-    ),
+    keywords=(),
     classifiers=[
-        'License :: OSI Approved :: Apache Software License',
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Science/Research',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ]
+        "License :: OSI Approved :: Apache Software License",
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Bio-Informatics",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
 )
