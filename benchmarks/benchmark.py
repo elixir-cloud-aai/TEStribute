14ee010f54fd9fd196bc1f8880ec73500fd6eb9a
vaniisgh <vanisingh@live.co.uk>
2019-09-02 18:58:16 +0400

    update benchmarks file
    update benchmarks file

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,8 +1,13 @@
+"""
+Module to setup TES and DRS instances as mentioned in config yaml file
+"""
 import os
 import yaml
 
 import logging
 from typing import Dict
+
+from bravado.exception import HTTPInternalServerError
 
 from drs_client import Client as drs_cli
 from tes_client import Client as tes_cli
@@ -12,40 +17,55 @@ from tes_client import Client as tes_cli
 
 
 def setup_drs(uri: 'string',objects: Dict):
+    """
+    :param uri: sting uri for the service to be updated
+    :param objects: list of objects that the db should be populated with
+    """
     drs_client = drs_cli(uri)
     drs_client.updateDatabaseObjects(objects,False)
-
     return "updated"
 
 
 def setup_tes(uri: 'string',costs: Dict):
+    """
+    :param uri: sting uri for the service to be updated
+    :param costs: values that the config should be populated with
+    """
     tes_client = tes_cli(uri)
     tes_client.updateTaskInfoConfig(costs["currency"],costs["time_unit"],costs["unit_costs"])
-
     return "updated"
 
 
 def setup_env(config_id: str):
+    """
+    :param config_id: id of file in the benchmarks folder according to which the config's of services should be setup
+    :return: Dict that can be passed as input to the TEStribute.rank_server() directly
+    """
+
     path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),"benchmark_configs/"+config_id))
     setup_dict = yaml.safe_load(open(path))
 
     try:
         drs_uris = setup_dict["drs_uris"]
-        try:
-            for uri in drs_uris:
+        for uri in drs_uris:
+            try:
                 setup_drs(uri, drs_uris[uri])
-        except KeyError:
-            print("No changes made to default config" + uri)
+            except KeyError :
+                print("No changes made to default config" + uri)
+            except HTTPInternalServerError:
+                print("Update endpoint not accessed :"+ uri)
     except KeyError:
         print("No DRS uris provided")
 
     try:
         tes_uris = setup_dict["tes_uris"]
-        try:
-            for uri in tes_uris:
+        for uri in tes_uris:
+            try:
                 setup_tes(uri, tes_uris[uri])
-        except KeyError:
-            print("No changes made to default config for " + uri)
+            except KeyError:
+                print("No changes made to default config for " + uri)
+            except HTTPInternalServerError:
+                print ("Update endpoint not accessed :" + uri)
     except KeyError:
         print("No TES uris provided")
 
