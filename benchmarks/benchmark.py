e8227054673b2d7a952d93bc203c5f50a3fbac13
Alex Kanitz <alexander.kanitz@unibas.ch>
2019-11-14 01:41:10 +0100

    fix: benchmark script

--- benchmarks/benchmark.py
+++ benchmarks/benchmark.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python3
+
 """
 Module to setup TES and DRS instances as mentioned in config yaml file
 """
+import logging
 import os
+import sys
+from typing import (Dict, List)
 import yaml
 
-import logging
-from typing import Dict
-
 from bravado.exception import HTTPInternalServerError
+import hiyapyco
 
 from drs_client import Client as drs_cli
 from tes_client import Client as tes_cli
@@ -16,11 +19,8 @@ from TEStribute.log import log_yaml
 from TEStribute.log import setup_logger
 from TEStribute import rank_services
 
+logger = setup_logger("TEStribute_benchmarks", logging.DEBUG)
 
-log_file = os.path.abspath(
-    os.path.join(os.path.dirname(os.path.realpath(__file__)), "log", "benchmarks.log")
-)
-logger = setup_logger("TEStribute_benchmarks", log_file, logging.DEBUG)
 
 def setup_drs(uri: str, objects: Dict):
     """
@@ -28,8 +28,10 @@ def setup_drs(uri: str, objects: Dict):
     :param objects: list of objects that the db should be populated with
     """
     drs_client = drs_cli(uri)
-    drs_client.updateDatabaseObjects(objects,False)
-    return "updated"
+    drs_client.updateDatabaseObjects(
+        objects=objects,
+        clear_db=True
+    )
 
 
 def setup_tes(uri: str, costs: Dict):
@@ -38,48 +40,92 @@ def setup_tes(uri: str, costs: Dict):
     :param costs: values that the config should be populated with
     """
     tes_client = tes_cli(uri)
-    tes_client.updateTaskInfoConfig(costs["currency"],costs["unit_costs"])
-    return "updated"
+    tes_client.updateTaskInfoConfig(
+        costs["currency"],
+        costs["unit_costs"]
+    )
 
 
-def setup_env(config_id: str):
+def setup_env(benchmarks: List[str]):
     """
     :param config_id: id of file in the benchmarks folder according to which the config's of services should be setup
     :return: Dict that can be passed as input to the TEStribute.rank_server() directly
     """
 
-    path = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__)),"benchmark_configs/"+config_id))
-    setup_dict = yaml.safe_load(open(path))
+    # Load benchmark
+    names = " ".join(benchmarks)
+    logger.info(f"Running benchmark: {names}")
+    paths = []
+    for benchmark in benchmarks:
+        paths.append(
+            os.path.abspath(
+                os.path.join(
+                    os.path.dirname(os.path.realpath(__file__)),
+                    "benchmarks",
+                    str(benchmark),
+                )
+            )
+        )
+    setup_dict = hiyapyco.load(
+        paths,
+        method=hiyapyco.METHOD_MERGE,
+        usedefaultyamlloader=True,
+        failonmissingfiles=True,
+    )
+    logger.debug(f"Benchmark config loaded.")
 
+    # Setup DRS instances
+    # TODO: Validate successful update
     try:
-        drs_uris = setup_dict["drs_uris"]
-        for uri in drs_uris:
+        for uri, values in setup_dict["drs_uris"].items():
             try:
-                setup_drs(uri, drs_uris[uri])
-            except KeyError :
-                logger.info("No changes made to default config" + uri)
+                logger.debug(f"Configuring DRS instance '{uri}'...")
+                setup_drs(uri, values)
             except HTTPInternalServerError:
-                logger.info("Update endpoint not accessed :"+ uri)
+                logger.warning(
+                    f"Update endpoint at service '{uri}' could not be accessed."
+                )
+            except Exception:
+                logger.warning(
+                    f"No changes made to default config for service '{uri}'."
+                )
     except KeyError:
-        print("No DRS uris provided")
+        logger.warning(
+            "No DRS URIs provided. No changed made to default configs."
+        )
 
+    # Setup TES instances
+    # TODO: Validate successful update
     try:
-        tes_uris = setup_dict["tes_uris"]
-        for uri in tes_uris:
+        for uri, values in setup_dict["tes_uris"].items():
             try:
-                setup_tes(uri, tes_uris[uri])
-            except KeyError:
-                logger.info("No changes made to default config for " + uri)
+                logger.debug(f"Configuring TES instance '{uri}'...")
+                setup_tes(uri, values)
             except HTTPInternalServerError:
-                logger.info("Update endpoint not accessed :" + uri)
+                logger.warning(
+                    f"Update endpoint at service '{uri}' could not be accessed."
+                )
+            except Exception:
+                logger.warning(
+                    f"No changes made to default config for service '{uri}'."
+                )
     except KeyError:
-        logger.info("No TES uris provided")
+        logger.warning(
+            "No DRS URIs provided. No changed made to default configs."
+        )
 
     return {
         "object_ids": setup_dict["object_ids"],
         "resource_requirements": setup_dict["resource_requirements"],
-        "tes_uris": list(tes_uris.keys()),
-        "drs_uris":list(drs_uris.keys()),
+        "tes_uris": list(setup_dict["tes_uris"].keys()),
+        "drs_uris": list(setup_dict["drs_uris"].keys()),
         "mode": setup_dict["mode"],
-        "auth_header": setup_dict["auth_header"],
+        "jwt": setup_dict["jwt"],
     }
+
+
+if __name__ == "__main__":
+    benchmarks = sys.argv[1:]
+    params = setup_env(benchmarks=benchmarks)
+    # TODO: alternatively use HTTP service
+    rank_services(**params)
