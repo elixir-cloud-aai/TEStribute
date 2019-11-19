168ace6cc5e9808475cbf9810d74a53d21a8aeff
juhani.kataja <juhani.kataja@csc.fi>
2019-11-19 15:37:07 +0100

    Require bash, instruct user to use <namespace>

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 LOCAL_TESTRIBUTE_IMAGE=${TESTRIBUTE_IMAGE:-elixircloud/testribute:latest}
 LOCAL_HOST_NAME=${TESTRIBUTE_HOST:-testribute}
@@ -71,7 +71,7 @@ EOF
 
 function usage() { # {{{
   echo -en "TEStribute deployer.\n"
-  echo -en "\nUsage:\n\t<envVar1>=<value1> <envVar2>=<value2> ... $0 -d | kubectl create -f -\n"
+  echo -en "\nUsage:\n\t<envVar1>=<value1> <envVar2>=<value2> ... $0 -d | kubectl -n <namespace> create -f -\n"
   echo -en "\nEnv vars:\n\tTESTRIBUTE_IMAGE: Use this image URI to run TEStribute.\n\tDefault: ${LOCAL_TESTRIBUTE_IMAGE}\n"
   echo -en "\n\tTESTRIBUTE_HOST: Expose TEStribute at this host.\n\tDefault: ${LOCAL_HOST_NAME}\n"
   echo -en "\n"
