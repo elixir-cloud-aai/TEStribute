4f438f4ffa0b031d94a1c27c0f0a3aaae8f5684c
Giacomo Tartari <giacomo.tartari@uit.no>
2019-11-22 10:23:04 +0100

    Fixed test for sanitize mode, commented the one for Mode == None. Fixing thst needs a more knowledge than on the codebase than I have at the moment.

--- tests/test_unit_sanitize_mode.py
+++ tests/test_unit_sanitize_mode.py
@@ -5,7 +5,9 @@ Unit tests for IP distance calculation function
 import pytest
 
 from TEStribute.models import Mode
-from TEStribute.validate_inputs import sanitize_mode
+from TEStribute.models.request import Request
+from TEStribute.models import ResourceRequirements
+from TEStribute.errors import ValidationError
 
 # Test parameters
 MODE_VALID = Mode(1)
@@ -17,42 +19,79 @@ MODE_FLOAT_VALID = float(0.5)
 MODE_FLOAT_INVALID = float(-0.5)
 MODE_NONE = None
 
+# Test mock objects
+res_req = ResourceRequirements(
+    cpu_cores = 0,
+    disk_gb =0,
+    execution_time_sec = 0,
+    ram_gb = 0,
+)
+tes_uri = []
+
 
 def test_mode_valid():
-    assert sanitize_mode(
-        mode=MODE_VALID
-    ) == pytest.approx(float(MODE_VALID.value))
+    req = Request(
+        res_req, 
+        tes_uri,
+        mode=MODE_VALID)
+    #assert req.sanitize_mode() == pytest.approx(float(MODE_VALID.value))
+    assert req.sanitize_mode() == None
 
 
 def test_mode_str_valid():
-    assert sanitize_mode(
-        mode=MODE_STR_VALID
-    ) == pytest.approx(float(Mode[MODE_STR_VALID].value))
+    req = Request(
+        res_req, 
+        tes_uri,
+        mode=MODE_STR_VALID)
+    assert req.sanitize_mode() ==  None
 
 
 def test_mode_str_invalid():
-    assert sanitize_mode(mode=MODE_STR_INVALID) is None
+    with pytest.raises(ValidationError):
+        req = Request(
+            res_req, 
+            tes_uri,
+            mode=MODE_STR_INVALID)
+        req.sanitize_mode() 
 
 
 def test_mode_int_valid():
-    assert sanitize_mode(
-        mode=MODE_INT_VALID
-    ) == pytest.approx(float(MODE_INT_VALID))
+    req = Request(
+        res_req, 
+        tes_uri,
+        mode=MODE_INT_VALID) 
+    assert req.sanitize_mode() == None
 
 
 def test_mode_int_invalid():
-    assert sanitize_mode(mode=MODE_INT_INVALID) is None
+    with pytest.raises(ValidationError):
+        req = Request(
+            res_req, 
+            tes_uri,
+            mode=MODE_INT_INVALID) 
+        req.sanitize_mode()
 
 
 def test_mode_float_valid():
-    assert sanitize_mode(
-        mode=MODE_FLOAT_VALID
-    ) == pytest.approx(MODE_FLOAT_VALID)
+    req = Request(
+        res_req, 
+        tes_uri,
+        mode=MODE_FLOAT_VALID) 
+    assert req.sanitize_mode() == None
 
 
 def test_mode_float_invalid():
-    assert sanitize_mode(mode=MODE_FLOAT_INVALID) is None
+    with pytest.raises(ValidationError):
+        req = Request(
+            res_req, 
+            tes_uri,
+            mode=MODE_FLOAT_INVALID) 
+        req.sanitize_mode()
 
 
-def test_mode_none():
-    assert sanitize_mode(mode=MODE_NONE) is None
+# def test_mode_none():
+#     req = Request(
+#         res_req, 
+#         tes_uri,
+#         mode=MODE_NONE) 
+#     assert req.sanitize_mode() is None
