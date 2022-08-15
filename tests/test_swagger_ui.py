b643263ac4dac60667d833583d73de952867a923
Ayush Kumar <64720666+Ayush5120@users.noreply.github.com>
2022-08-15 14:54:58 +0530

    test: check if Swagger UI is available (#94)

---
+++ tests/test_swagger_ui.py
@@ -0,0 +1,13 @@
+"""Integration test to check whether swagger ui is available."""
+
+import requests
+
+UI_URL = "http://localhost:7979/ui"
+
+
+def test_swagger_ui_200():
+    """Test for swagger ui."""
+    response = requests.get(
+        url=UI_URL
+    )
+    assert response.status_code == 200
